69e31c8890cc8fa3a8a30e1cdec34c074bf78c6e
javking-pranesh <javkingpranesh18@gmail.com>
2025-10-02 19:16:01 +0530

    Add BrowserRouter and Created Login Page

--- netflix-clone/src/pages/Login/Login.jsx
+++ netflix-clone/src/pages/Login/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
+
 const Login = () => {
+
+  const [signInState , setSignInState]= useState("Sign In");
+
   return (
     <div className='login'>
-      
+      <img src={logo} alt="" />
+      <div className="login-form">
+        <h1>{signInState}</h1>
+        <form action="">
+          {signInState === "Sign Up" ? <input type="text" placeholder='Your Name'/> : <></>}
+          <input type="email" placeholder='Email'/>
+          <input type="password" placeholder='Password'/>
+          <button>{signInState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox"/>
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+
+          {
+            signInState=== "Sign In" 
+            ? <p>New to Netflix? <span onClick={() =>{setSignInState("Sign Up")}}>Sign Up Now</span></p> 
+            : <p>Already have account? <span onClick={() =>{setSignInState("Sign In")}}>Sign In Now</span ></p>
+          }
+          
+          
+        </div>
+      </div>
     </div>
   )
 }
