312257c331c3615d582a737a2e0286dac570b22d
javking-pranesh <javkingpranesh18@gmail.com>
2025-10-02 21:33:35 +0530

    Updated Data from TMDB API

--- netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -1,28 +1,59 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data';
 
 const TitleCards = ({title,category}) => {
   const cardsRef = useRef();
+  const [apiData,setApiData] = useState([]);
 
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzhlNGIzOWM3MzNkY2FjYjVjNDY5NDU4ZWVhNWRhNCIsIm5iZiI6MTc1OTQxNzU1Mi44NjcsInN1YiI6IjY4ZGU5NGQwNWY2Y2M3ODUyZTZhNzliZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u_WvJGfdKifUJzwSdBYQZFJZacYz2EhWoQG9i3YJC4M'
+  }
+};
+
+useEffect(() =>{
+  fetch(`https://api.themoviedb.org/3/movie/${category? category : "now_playing"}?language=en-US&page=1`, options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results))
+  .catch(err => console.error(err));
+
+},[category])
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
 
   return (
-    <div className='titlecards'>
-      <h2>{title?title:"Popular on Netflix"}</h2>
-      <div className="card-list" ref={cardsRef} onWheel={handleWheel}>
-        {cards_data.map((card, index) => (
-          <div className="card" key={index}>
-            <img src={card.image} alt={card.name} />
-            <p>{card.name}</p>
-          </div>
-        ))}
-      </div>
+  <div className='titlecards'>
+    <h2>{title ? title : "Popular on Netflix"}</h2>
+    <div className="card-list" ref={cardsRef} onWheel={handleWheel}>
+      {category 
+        ? (
+          apiData.map((card, index) => (
+            <div className="card" key={index}>
+              <img 
+                src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`} 
+                alt={card.original_title || card.name} 
+              />
+              <p>{card.original_title || card.name}</p>
+            </div>
+          ))
+        ) : (
+          cards_data.map((card, index) => (
+            <div className="card" key={index}>
+              <img src={card.image} alt={card.name} />
+              <p>{card.name}</p>
+            </div>
+          ))
+        )
+      }
     </div>
-  );
+  </div>
+);
+
 };
 
 export default TitleCards;
--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -25,10 +25,10 @@ const Home = () => {
         </div>
       </div>
       <div className="more-cards">
-         <TitleCards title={"Blockbuster Movies"}/>
-          <TitleCards title={"Only on Netflix"}/>
-          <TitleCards title={"Upcoming"}/>
-          <TitleCards title={"Top picks for You"}/>
+         <TitleCards title={"Blockbuster Movies"} category={"popular"}/>
+          <TitleCards title={"Only on Netflix"} category={"top_rated"}/>
+          <TitleCards title={"Upcoming"} category={"upcoming"}/>
+          <TitleCards title={"Top picks for You"} category={"now_playing"}/>
         </div>
         <Footer/>
     </div>
